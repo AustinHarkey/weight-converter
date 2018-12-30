c19e5e11ef1563b842b796410baf652965ef9451
Austin Harkey <austindharkey@gmail.com>
2018-12-29 21:48:19 -0800

    v1 working app

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let input = document.querySelector('.form__input');
+let output = document.querySelector('.output');
+output.style.display = 'none'
 
 input.addEventListener('input', function(e) {
     let pounds = e.target.value;
@@ -7,8 +9,14 @@ input.addEventListener('input', function(e) {
     let kiloOutput = document.getElementById('output__kilograms');
     let ouncesOutput = document.getElementById('output__ounces');
 
-    gramsOutput.innerHTML = pounds/0.0022046;
-    kiloOutput.innerHTML = pounds/2.2046;
-    ouncesOutput.innerHTML = pounds*16;
+    gramsOutput.innerHTML = (pounds/0.0022046).toFixed(2);
+    kiloOutput.innerHTML = (pounds/2.2046).toFixed(2);
+    ouncesOutput.innerHTML = (pounds*16).toFixed(2);
+
+    output.style.display = 'block';
+
+    if (e.target.value == '') {
+        output.style.display = 'none';
+    }
   
 });
